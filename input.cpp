1312dca485501c81d7c3ea2982c51823fe52ce1e
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-04-24 10:43:50 +0330

    Create input.cpp

---
+++ input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Enter your number: ";
+
+    int value;
+    cin >> value;
+    cout << value << endl;
+
+    cout << "Enter x and y: " << endl;
+
+    double x , y;
+    cin >> x >> y;
+    cout << x+y << endl;
+
+    return 0;
+}
