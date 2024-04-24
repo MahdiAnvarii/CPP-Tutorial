6bb81c2e4a1367b57afa24e852da23f86e30c6bb
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-04-24 10:41:48 +0330

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int times = 100;
+    const double pi = 3.14;
+    std::cout << "Hello World, " << times << " times" << std::endl;
+    std::cout << pi << std::endl;
+    return 0;
+}
