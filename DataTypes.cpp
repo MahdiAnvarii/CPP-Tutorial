965dc220aeff146aff7f501ad7868d7ad68d8085
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-04-24 11:00:56 +0330

    Create DataTypes.cpp

---
+++ DataTypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double n1 = 99.999;
+    float n2 = 3.14F;
+    long n3 = 9000000L;
+
+    char l1 = 'f';
+    bool l2 = true;
+    auto l3 = false;
+
+    int number {};
+    cout << number << endl;
+
+    return 0;
+}
