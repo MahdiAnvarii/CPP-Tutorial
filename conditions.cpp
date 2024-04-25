d7bf1f5db153389e18e4e518ba8a34b5e549888e
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-04-25 15:51:47 +0330

    Create conditions.cpp

---
+++ conditions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+
+    if (a<10){
+        cout << "Hi" << endl;
+    }
+    else if (a<10){
+        cout << "Bye" << endl;
+    }
+    else{
+        cout << "HiBye" << endl;
+    }
+}
