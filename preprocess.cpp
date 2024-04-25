d8700d708be237d0b10bb46bcb1aa10835261469
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-04-25 11:24:15 +0330

    Create preprocess.cpp

---
+++ preprocess.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define INTEGER int
+
+#if 1
+INTEGER main(){
+    INTEGER a;
+    std::cin >> a;
+    INTEGER b;
+    std::cin >> b;
+    INTEGER result = a * b;
+    std::cout << result << std::endl;
+    return result;
+#include "brace.h"
+#endif
